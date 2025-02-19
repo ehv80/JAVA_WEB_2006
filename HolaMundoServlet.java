fcc5ea9e4c7d5208bff1a284c05e870999bebc5e
ehv80 <ezequiel.villanueva@gmail.com>
2025-02-19 10:08:03 -0300

    Modificación de .classpath y de HolaMundoServlet.java

--- HolaMundoServlet.java
+++ HolaMundoServlet.java
@@ -9,10 +9,14 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.ServletException;
+import jakarta.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 
 /* 
  * Extendemos la clase HttpServlet para definir
