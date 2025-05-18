1e08174b0b8f495efe06d0b7a6e7165d27222268
ehv80 <ezequiel.villanueva@gmail.com>
2025-05-18 11:15:12 -0300

    Creación del archivo 007_ActionForms.txt y carpeta del proyecto tomcat ActionForms

---
+++ ActionForms/WEB-INF/src/DatosForm.java
@@ -0,0 +1,9 @@
+//package capítulo5; //comentado para evitar error de clase o paquete no encontrado
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionForm;
+
+public class DatosForm {
+
+}
