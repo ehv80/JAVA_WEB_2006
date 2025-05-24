22f5a05d906a92d568e284bfdb2b49a19d793cc6
ehv80 <ezequiel.villanueva@gmail.com>
2025-05-24 10:36:54 -0300

    Modificación de DatosForm.java y 007_ActionForms.txt

--- ActionForms/WEB-INF/src/DatosForm.java
+++ ActionForms/WEB-INF/src/DatosForm.java
@@ -3,7 +3,53 @@
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
 
-public class DatosForm {
+public class DatosForm extends ActionForm 
+{
+	private String nombre;
+	private String sexo;
+	private Integer edad;
+	private boolean fumador;
+	
+	public String getNombre()
+	{
+		return this.nombre;
+	}
+	
+	public void setNombre(String nombre)
+	{
+		this.nombre = nombre;
+	}
+	
+	public String getSexo()
+	{
+		return this.sexo;
+	}
+	
+	public void setSexo(String sexo)
+	{
+		this.sexo = sexo;
+	}
+	
+	public Integer getEdad()
+	{
+		return this.edad;
+	}
+	
+	public void setEdad(Integer edad)
+	{
+		this.edad = edad;
+	}
+	
+	public boolean isFumador()
+	{
+		return this.fumador;
+	}
+	
+	public void setFumador(boolean fumador)
+	{
+		this.fumador = fumador;
+	}
 
 }
