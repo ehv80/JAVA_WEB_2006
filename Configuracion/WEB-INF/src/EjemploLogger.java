4c11c7be96396762647fc4dcc9fdd71896805d48
ehv80 <ezequiel.villanueva@gmail.com>
2026-06-16 10:40:06 -0300

    Modificación de 012_Configuracion_y_extension_de_Struts.txt y EjemploLogger.java

--- Configuracion/WEB-INF/src/EjemploLogger.java
+++ Configuracion/WEB-INF/src/EjemploLogger.java
@@ -5,28 +5,28 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
 
-public class ejemplo-logger extends Action 
-{
+public class EjemploLogger extends Action {
 
-    private static final Log log = LogFactory.getLog(ejemplo-logger.class);
+    private static final Log log = LogFactory.getLog(EjemploLogger.class);
 
     public ActionForward execute(ActionMapping map, ActionForm form,
             HttpServletRequest req, HttpServletResponse res)
-            throws Exception 
-    {
-        log.trace("Inicia la acción ejemplo-logger");
-        
-        try
+            throws Exception {
+        log.trace("Inicia la acción EjemploLogger");
+
+        try 
         {
             int edad = Integer.parseInt(req.getParameter("edad"));
-        }
-        catch (NumberFormatException nfe)
+        } 
+        catch (NumberFormatException nfe) 
         {
             log.warn("Edad inválida", nfe);
         }
 
         return map.findForward("ok");
     }
-    
+
 }
